301d2cd3e079947e138c1cd844c31adea22e0208
Olha Svatukha <olha.s2018@gmail.com>
2021-03-28 21:53:08 +0300

    deleted js code

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-const toDown = document.querySelector('.down');
 
-window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-
-}
-
-function downPage() {
-    document.body.scrollDown = 200;
-    document.documentElement.scrollDown = 200;
-}
 
 
 
