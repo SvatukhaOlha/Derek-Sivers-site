78faccf70ed6fafed8ed4a53e374e79997553f6b
Olha Svatukha <olha.s2018@gmail.com>
2021-03-28 21:49:32 +0300

    day 1

---
+++ index.js
@@ -0,0 +1,15 @@
+const toDown = document.querySelector('.down');
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+
+}
+
+function downPage() {
+    document.body.scrollDown = 200;
+    document.documentElement.scrollDown = 200;
+}
+
+
+
